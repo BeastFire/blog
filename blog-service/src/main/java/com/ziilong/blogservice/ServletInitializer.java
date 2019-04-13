0c9313aa127432183ba08865f34a395e6b77ece8
LongWang <476403337@qq.com>
2019-04-13 13:00:15 +0800

    blog-service init

---
+++ blog-service/src/main/java/com/ziilong/blogservice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ziilong.blogservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BlogServiceApplication.class);
+	}
+
+}
